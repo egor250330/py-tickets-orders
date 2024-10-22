936bccdd359628118ab5ee762d358862b875bbbf
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-10-22 22:42:27 +0300

    changed views.py

--- cinema/views.py
+++ cinema/views.py
@@ -85,8 +85,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             queryset = queryset.prefetch_related("tickets").annotate(
                 tickets_available=(
-                        F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
-                        - Count("tickets")
+                    F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
+                    - Count("tickets")
                 )
             )
 
